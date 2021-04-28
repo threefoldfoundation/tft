462b8b37dc2e00db67cc580ba55676feae41fb23
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-04-28 10:44:37 +0200

    Fix proxy contract

--- solidity/contract/proxy.sol
+++ solidity/contract/proxy.sol
@@ -5,18 +5,17 @@ import "./token_storage.sol";
 // inherit from TokenStorage so we have the constructor, since the token variables need to be stored in the
 // proxy's storage
 contract Proxy is TokenStorage {
-    receive () external payable {
+    fallback () external payable {
         // directly get the implementation contract address from the storage. This way we don't need to depend
         // on the upgradeable contract
         address _impl = getAddress(keccak256(abi.encode("implementation")));
         require(_impl != address(0), "The implementation address can't be the zero address");
+        bytes memory data = msg.data;
 
         assembly {
-            let ptr := mload(0x40)
-            calldatacopy(ptr, returndatasize(), calldatasize())
-
-            let result := delegatecall(gas(), _impl, ptr, calldatasize(), returndatasize(), returndatasize())
+            let result := delegatecall(gas(), _impl, add(data, 0x20), mload(data), 0, 0)
             let size := returndatasize()
+            let ptr := mload(0x40)
             returndatacopy(ptr, 0, size)
             switch result
             case 0 { revert(ptr, size) }
@@ -24,10 +23,10 @@ contract Proxy is TokenStorage {
         }
     }
 
-    constructor() public {
+    constructor() {
         //set initial contract address, needs to be hardcoded
         // TODO: Set correct address
-        address impl_addr = address(0xDAD7A460EA562e28fB90cF524B62ea4cBc1685af);
+        address impl_addr = address(0);
         require(impl_addr != address(0), "implementation address can not be the zero address");
         setAddress(keccak256(abi.encode("implementation")), impl_addr);
         setString(keccak256(abi.encode("version")),"0");
