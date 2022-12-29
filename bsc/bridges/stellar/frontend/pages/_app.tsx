3389723dab54ce6830c591d23513bb9cef80691d
dylanVerstraete <dylan_verstraete1@hotmail.com>
2022-12-29 15:08:10 +0100

    feat: add crisp extension to UI

--- bsc/bridges/stellar/frontend/pages/_app.tsx
+++ bsc/bridges/stellar/frontend/pages/_app.tsx
@@ -4,6 +4,21 @@ import Head from 'next/head'
 import '../styles.css'
 
 export default class Root extends App {
+  componentDidMount(): void {
+    // Include the Crisp code here, without the <script></script> tags
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "1a5a5241-91cb-4a41-8323-5ba5ec574da0";
+
+    (function() {
+      var d = document;
+      var s = d.createElement("script");
+
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  }
+
   render() {
     const { Component } = this.props
 
