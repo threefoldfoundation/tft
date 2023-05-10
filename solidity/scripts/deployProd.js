e27251bb89fd4cb00df21f396757c4354ea7b414
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-05-10 16:47:07 +0200

    Use the networkid from the arguments in the deployProd script

--- solidity/scripts/deployProd.js
+++ solidity/scripts/deployProd.js
@@ -1,24 +1,25 @@
 const { ethers } = require("hardhat");
 
-async function main () {
+async function main() {
   const args = process.argv
 
   if (args.length < 5) {
     console.log("Usage: deployProd.js <private key> <provider url> <network id>")
+    console.log("Common networkIds:  mainnet: 1, goerli-testnet: 5, sepolia-testnet: 11155111")
     process.exit(1)
   }
 
   const privateKey = args[2]
-  
+
 
   const providerUrl = args[3]
   console.log("providerUrl=", providerUrl)
 
-  const networkId = args[4]
+  const networkId = parseInt(args[4])
   console.log("networkId=", networkId)
 
-  const provider = new ethers.providers.WebSocketProvider(providerUrl, 5)
-  
+  const provider = new ethers.providers.WebSocketProvider(providerUrl, networkId)
+
   console.log(await provider.getNetwork())
 
   const wallet = new ethers.Wallet(privateKey).connect(provider);
@@ -31,10 +32,10 @@ async function main () {
   await token.deployed()
   console.log('Token deployed to:', token.address);
 }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
